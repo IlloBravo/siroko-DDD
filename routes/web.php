5679fc860f401b798bb24e24e9960ef7062bdd4c
IlloBravo <bravodearcedavid@gmail.com>
2024-12-11 17:56:16 +0100

    seems all done, lets create readme.md

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () {
+    return redirect('/all-products-available');
+});
+
 Route::get('/all-products-available', [ProductController::class, 'index'])->name('products.index');
 
 Route::get('/all-carts-created', [CartController::class, 'index'])->name('cart.index');
