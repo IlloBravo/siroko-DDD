899cd2463d0530c4c2cf13d87fcbc79589036ab7
IlloBravo <bravodearcedavid@gmail.com>
2024-12-11 10:11:58 +0100

    refactor: deleted Cart repository in Product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,23 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Domain\Cart\Repository\CartRepositoryInterface;
 use App\Domain\Product\Repository\ProductRepositoryInterface;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
     public function __construct(
-        private readonly ProductRepositoryInterface $productRepository,
-        private readonly CartRepositoryInterface $cartRepository
+        private readonly ProductRepositoryInterface $productRepository
     ) {}
 
     public function index(): View
     {
         $products = $this->productRepository->findAll();
 
-        $cartId = session('cart_id');
-
-        return view('product.index', compact('products', 'cartId'));
+        return view('product.index', compact('products'));
     }
 }
