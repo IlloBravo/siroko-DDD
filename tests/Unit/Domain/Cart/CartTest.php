0c1488bc6993a551ccd8f9124e71e8c30df88403
IlloBravo <bravodearcedavid@gmail.com>
2024-12-14 13:26:23 +0100

    tests and tests

--- tests/Unit/Domain/Cart/CartTest.php
+++ tests/Unit/Domain/Cart/CartTest.php
@@ -3,261 +3,178 @@
 namespace Tests\Unit\Domain\Cart;
 
 use App\Domain\Cart\Cart;
+use App\Domain\Cart\CartItem;
 use App\Domain\Product\Product;
+use App\Domain\Product\Repository\ProductRepositoryInterface;
 use App\Domain\Shared\ValueObjects\UuidVO;
-use DateMalformedStringException;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid;
 
 class CartTest extends TestCase
 {
-    /**
-     * @throws DateMalformedStringException
-     */
-    public function testAddProduct()
-    {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        $cart = Cart::create($cartData);
-
-        $productData = (object) [
-            'id' => '123e4567-e89b-12d3-a456-426614174001',
-            'name' => 'Test Product',
-            'price' => 100.0,
-            'quantity' => 10,
-            'cartQuantity' => 0,
-        ];
-        $product = Product::fromDatabase($productData);
-
-        $cart->addProduct($product, 2);
-
-        $this->assertEquals(1, $cart->items->count());
-        $this->assertEquals(8, $product->stock);
-
-        $cart->addProduct($product, 3);
-        $this->assertEquals(5, $product->stock);
-        $this->assertEquals(5, $cart->getProductQuantity(UuidVO::fromString($product->id)));
-    }
+    private Cart $cart;
+    private CartItem $cartItem1;
+    private CartItem $cartItem2;
 
     /**
-     * @throws DateMalformedStringException
+     * @throws Exception
      */
-    public function testUpdateProductQuantity()
+    protected function setUp(): void
     {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        $cart = Cart::create($cartData);
+        parent::setUp();
 
         $productData = (object) [
-            'id' => '123e4567-e89b-12d3-a456-426614174001',
-            'name' => 'Gafas de Sol',
-            'price' => 59.99,
-            'quantity' => 10,
-            'cartQuantity' => 0,
+            'id' => UuidVO::generate()->__toString(),
+            'name' => 'Bike',
+            'price' => 1500.00,
+            'stock' => 10,
         ];
-        $product = Product::fromDatabase($productData);
 
-        $cart->addProduct($product, 2);
-        $cart->updateProductQuantity(UuidVO::fromString($product->id), 4);
-
-        $this->assertEquals(6, $product->stock);
-        $this->assertEquals(4, $cart->getProductQuantity(UuidVO::fromString($product->id)));
-
-        $cart->updateProductQuantity(UuidVO::fromString($product->id), 1);
-        $this->assertEquals(9, $product->stock);
-        $this->assertEquals(1, $cart->getProductQuantity(UuidVO::fromString($product->id)));
-    }
-
-    /**
-     * @throws DateMalformedStringException
-     */
-    public function testRemoveProduct()
-    {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        $cart = Cart::create($cartData);
-
-        $productData = (object) [
-            'id' => '123e4567-e89b-12d3-a456-426614174001',
-            'name' => 'Producto B',
-            'price' => 30.0,
-            'quantity' => 20,
-            'cartQuantity' => 0,
-        ];
-        $product = Product::fromDatabase($productData);
-
-        $cart->addProduct($product, 5);
-
-        $this->assertEquals(5, $cart->getProductQuantity(UuidVO::fromString($product->id)));
-
-        $cart->removeProduct(UuidVO::fromString($product->id));
-
-        $this->assertEquals(20, $product->stock);
-        $this->assertEquals(0, $cart->getProductQuantity(UuidVO::fromString($product->id)));
-    }
-
-    public function testFromDatabase()
-    {
+        $cartId = UuidVO::generate()->__toString();
         $cartData = (object) [
-            'id' => '123e4567-e89b-12d3-a456-426614174000',
+            'id' => $cartId,
             'items' => json_encode([]),
-            'created_at' => '2024-12-10 12:00:00',
-            'updated_at' => '2024-12-10 12:00:00',
+        ];
+
+        $this->cart = Cart::fromDatabase($cartData);
+
+        $cartItemData1 = (object) [
+            'id' => UuidVO::generate()->__toString(),
+            'cart' => $cartData,
+            'product' => $productData,
+            'quantity' => 2,
+        ];
+
+        $cartItemData2 = (object) [
+            'id' => UuidVO::generate()->__toString(),
+            'cart' => $cartData,
+            'product' => $productData,
+            'quantity' => 4,
+        ];
+
+        $this->cartItem1 = CartItem::fromDatabase($cartItemData1);
+        $this->cartItem2 = CartItem::fromDatabase($cartItemData2);
+
+        $this->mockProductRepository($productData);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function mockProductRepository(object $productData): void
+    {
+        $mockRepository = $this->createMock(ProductRepositoryInterface::class);
+
+        $mockRepository
+            ->method('findByIdOrFail')
+            ->willReturn(Product::fromDatabase($productData));
+
+        app()->instance(ProductRepositoryInterface::class, $mockRepository);
+    }
+
+    public function testCartCanBeCreatedFromDatabase(): void
+    {
+        $this->assertInstanceOf(Cart::class, $this->cart);
+        $this->assertCount(0, $this->cart->getCartItems());
+    }
+
+    public function testCanAddCartItem(): void
+    {
+        $this->cart->addCartItem($this->cartItem1, 2);
+
+        $this->assertCount(1, $this->cart->getCartItems());
+        $this->assertEquals(2, $this->cart->getCartItems()->first()->quantity);
+    }
+
+    public function testCanUpdateCartItemQuantity(): void
+    {
+        $this->cart->addCartItem($this->cartItem1, 0);
+
+        $this->cart->updateCartItemQuantity($this->cartItem1, 10);
+
+        $this->assertEquals(10, $this->cart->getCartItems()->first()->quantity);
+    }
+
+    public function testCanRemoveCartItem(): void
+    {
+        $this->cart->addCartItem($this->cartItem1, 0);
+        $this->cart->removeCartItem($this->cartItem1->id);
+
+        $this->assertCount(0, $this->cart->getCartItems());
+    }
+
+    public function testCartCheckout(): void
+    {
+        $this->cart->addCartItem($this->cartItem1, 0);
+        $this->cart->addCartItem($this->cartItem2, 0);
+
+        $this->cart->checkout();
+
+        $this->assertCount(0, $this->cart->getCartItems());
+    }
+
+    public function testCartTotal(): void
+    {
+        $this->cart->addCartItem($this->cartItem1, 0);
+        $this->cart->addCartItem($this->cartItem2, 0);
+
+        $this->assertEquals(3000.00, $this->cart->total());
+    }
+
+    public function testCartTotalWhenEmpty(): void
+    {
+        $this->assertEquals(0.00, $this->cart->total());
+    }
+
+    public function testCartCanBeInstantiatedWithNoItems(): void
+    {
+        $cartId = UuidVO::generate()->__toString();
+        $cartData = (object) [
+            'id' => $cartId,
+            'items' => json_encode([]),
         ];
 
         $cart = Cart::fromDatabase($cartData);
 
-        $this->assertEquals('123e4567-e89b-12d3-a456-426614174000', (string) $cart->id);
+        $this->assertInstanceOf(Cart::class, $cart);
+        $this->assertEquals($cartId, $cart->id->__toString());
+        $this->assertCount(0, $cart->getCartItems());
     }
 
-    public function testGetTotalProducts()
+    public function testCartItemsAreProperlyInstantiatedFromValidDatabaseData(): void
     {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
+        $cartId = UuidVO::generate()->__toString();
+        $productId1 = UuidVO::generate()->__toString();
+        $productId2 = UuidVO::generate()->__toString();
+
+        $cartData = (object) [
+            'id' => $cartId,
+            'items' => json_encode([
+                [
+                    'id' => UuidVO::generate()->__toString(),
+                    'cartId' => $cartId,
+                    'productId' => $productId1,
+                    'quantity' => 3,
+                ],
+                [
+                    'id' => UuidVO::generate()->__toString(),
+                    'cartId' => $cartId,
+                    'productId' => $productId2,
+                    'quantity' => 4,
+                ],
+            ]),
         ];
-        $cart = Cart::create($cartData);
 
-        $productData = (object) [
-            'id' => Uuid::uuid4(),
-            'name' => 'Camiseta',
-            'price' => 20.0,
-            'quantity' => 15,
-            'cartQuantity' => 0,
-        ];
-        $product = Product::fromDatabase($productData);
+        $cart = Cart::fromDatabase($cartData);
 
-        $cart->addProduct($product, 3);
+        $firstItem = $cart->getCartItems()->first();
+        $this->assertEquals($cartId, $firstItem->cartId->__toString());
+        $this->assertEquals($productId1, $firstItem->productId->__toString());
+        $this->assertEquals(3, $firstItem->quantity);
 
-        $this->assertEquals(3, $cart->getTotalProducts());
-    }
-
-    public function testGetProductQuantity()
-    {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        $cart = Cart::create($cartData);
-
-        $productData = (object) [
-            'id' => '123e4567-e89b-12d3-a456-426614174001',
-            'name' => 'Producto A',
-            'price' => 50.0,
-            'quantity' => 10,
-            'cartQuantity' => 0,
-        ];
-        $product = Product::fromDatabase($productData);
-
-        $cart->addProduct($product, 5);
-
-        $this->assertEquals(5, $cart->getProductQuantity(UuidVO::fromString($product->id)));
-
-        $nonExistentProductId = UuidVO::fromString(Uuid::uuid4()->toString());
-        $this->assertEquals(0, $cart->getProductQuantity($nonExistentProductId));
-    }
-
-    public function testCartIsEmptyAfterCheckout()
-    {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        $cart = Cart::create($cartData);
-
-        $productData = (object) [
-            'id' => Uuid::uuid4(),
-            'name' => 'Gorra',
-            'price' => 15.0,
-            'quantity' => 10,
-            'cartQuantity' => 0,
-        ];
-        $product = Product::fromDatabase($productData);
-
-        $cart->addProduct($product, 2);
-        $cart->checkout();
-
-        $this->assertEmpty($cart->items);
-    }
-
-    /**
-     * @throws DateMalformedStringException
-     */
-    public function testGetProduct()
-    {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        $cart = Cart::create($cartData);
-
-        $productData = (object) [
-            'id' => '123e4567-e89b-12d3-a456-426614174001',
-            'name' => 'Producto A',
-            'price' => 50.0,
-            'quantity' => 10,
-            'cartQuantity' => 0,
-        ];
-        $product = Product::fromDatabase($productData);
-
-        $cart->addProduct($product, 5);
-
-        $retrievedProduct = $cart->getProduct(UuidVO::fromString($product->id));
-        $this->assertNotNull($retrievedProduct);
-        $this->assertEquals('Producto A', $retrievedProduct->name);
-
-        $nonExistentProductId = UuidVO::fromString(Uuid::uuid4()->toString());
-        $retrievedProduct = $cart->getProduct($nonExistentProductId);
-        $this->assertNull($retrievedProduct);
-    }
-
-    /**
-     * @throws DateMalformedStringException
-     */
-    public function testGetProductStock()
-    {
-        $cartData = [
-            'id' => Uuid::uuid4(),
-            'items' => json_encode([]),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        $cart = Cart::create($cartData);
-
-        $productData = (object) [
-            'id' => '123e4567-e89b-12d3-a456-426614174001',
-            'name' => 'Producto B',
-            'price' => 30.0,
-            'quantity' => 20,
-            'cartQuantity' => 0,
-        ];
-        $product = Product::fromDatabase($productData);
-
-        $cart->addProduct($product, 5);
-
-        $this->assertEquals(20, $cart->getProductStock(UuidVO::fromString($product->id)));
-
-        $nonExistentProductId = UuidVO::fromString(Uuid::uuid4()->toString());
-        $this->assertEquals(0, $cart->getProductStock($nonExistentProductId));
+        $secondItem = $cart->getCartItems()->last();
+        $this->assertEquals($cartId, $secondItem->cartId->__toString());
+        $this->assertEquals($productId2, $secondItem->productId->__toString());
+        $this->assertEquals(4, $secondItem->quantity);
     }
 }
