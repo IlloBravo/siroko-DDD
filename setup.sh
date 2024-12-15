f93c247973c57ff9d30cfe87b813602e1765da3a
IlloBravo <bravodearcedavid@gmail.com>
2024-12-15 18:45:28 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ php artisan key:generate
 echo "🗄️ Configurando la base de datos SQLite..."
 touch database/database.sqlite
 
+# Crear el archivo .env si no existe
 if [ ! -f .env ]; then
     echo "📝 Creando archivo .env..."
     cp .env.example .env
@@ -18,13 +19,45 @@ if [ ! -f .env ]; then
     sed -i 's/DB_DATABASE=.*/DB_DATABASE=database\/database.sqlite/' .env
 fi
 
+# Crear el archivo .env.testing si no existe
+if [ ! -f .env.testing ]; then
+    echo "📝 Creando archivo .env.testing..."
+    cat > .env.testing <<EOL
+APP_NAME=Laravel
+APP_ENV=testing
+APP_KEY=$(php artisan key:generate --show)
+APP_DEBUG=true
+APP_TIMEZONE=UTC
+APP_URL=http://localhost
+
+APP_LOCALE=es
+APP_FALLBACK_LOCALE=en
+APP_FAKER_LOCALE=es_ES
+
+LOG_CHANNEL=stack
+LOG_STACK=single
+LOG_DEPRECATIONS_CHANNEL=null
+LOG_LEVEL=debug
+
+DB_CONNECTION=sqlite
+DB_DATABASE=database/testing.sqlite
+
+SESSION_DRIVER=array
+QUEUE_CONNECTION=sync
+CACHE_STORE=array
+EOL
+fi
+
+echo "🗄️ Configurando la base de datos de test SQLite..."
+touch database/testing.sqlite
+
 echo "⚙️ Ejecutando migraciones y seeders..."
-php artisan migrate:fresh && php artisan db:seed
+php artisan migrate:fresh --seed
 
-echo "⚙️ Ejecutando migraciones y seeders para el entorno de test"
-php artisan migrate:fresh && php artisan db:seed --env=testing
+echo "⚙️ Ejecutando migraciones y seeders para el entorno de test..."
+php artisan migrate:fresh --seed --env=testing
 
-echo "⚙️ Ejecutando tests"
+echo "⚙️ Ejecutando tests..."
 php artisan test
 
 echo "🌐 Iniciando el servidor de desarrollo en http://127.0.0.1:8000..."
